0e6b0a1e735f0450c58b7bf24e3f1a863a02b3b1
Erika Blom <erikablom13@gmail.com>
2023-02-26 16:15:54 +0100

    Exercise 5: Keyboard and screen reader support

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import Toast from '../Toast';
 import styles from './ToastShelf.module.css';
@@ -8,8 +8,28 @@ import { ToastContext } from '../ToastProvider';
 function ToastShelf() {
   const { toastArray, onDismiss } = useContext(ToastContext);
 
+  useEffect(() => {
+    const dismissToasts = (e) => {
+      if (e.code === 'Escape')  {
+        toastArray.map(toast => {
+          return onDismiss(toast.id);
+        })
+      }
+    }
+    
+    window.addEventListener('keydown', dismissToasts);
+
+    return () => {
+      window.removeEventListener('keydown', dismissToasts);
+    }
+  }, [onDismiss, toastArray]);
+
   return (
-    <ol className={styles.wrapper}>
+    <ol 
+      className={styles.wrapper} 
+      role="region"
+      aria-live="assertive"
+      aria-label="Notification">
       {toastArray.map(toast =>  
       <li key={toast.id} className={styles.toastWrapper}>
         <Toast variant={toast.variant} dismiss={() => onDismiss(toast.id)}>{toast.message}</Toast>
